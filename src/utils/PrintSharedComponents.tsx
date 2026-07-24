6398b1fc611d43d2682278a50b789348d21ba996
ferasjaz24 <meliodass715@gmail.com>
2026-07-25 01:48:36 +0300

    START UP

---
+++ src/utils/PrintSharedComponents.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const DocumentHeader = () => (
+  <div style={{ width: '100%', textAlign: 'center', marginBottom: '24px', userSelect: 'none' }}>
+    <img src="https://i.postimg.cc/BZ1y7kQW/HEEDER-NEXT-PAGE-4x.png" referrerPolicy="no-referrer" alt="Header" style={{ width: '100%', maxHeight: '120px', objectFit: 'contain' }} />
+  </div>
+);
+
+export const DocumentFooter = () => (
+  <div style={{ width: '100%', textAlign: 'center', marginTop: 'auto', paddingTop: '16px', userSelect: 'none' }}>
+    <img src="https://i.postimg.cc/pXfSRjFk/tdyyl-NEXTPAGE-4x.png" referrerPolicy="no-referrer" alt="Footer" style={{ width: '100%', maxHeight: '120px', objectFit: 'contain' }} />
+  </div>
+);
